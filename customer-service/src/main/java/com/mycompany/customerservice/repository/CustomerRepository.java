150103babff80d3fee16dda394873daf1e6fab13
Colin But <colinbut@users.noreply.github.com>
2017-02-24 21:36:51 +0000

    repository

---
+++ customer-service/src/main/java/com/mycompany/customerservice/repository/CustomerRepository.java
@@ -0,0 +1,14 @@
+/*
+ * |-------------------------------------------------
+ * | Copyright © 2017 Colin But. All rights reserved. 
+ * |-------------------------------------------------
+ */
+package com.mycompany.customerservice.repository;
+
+import com.mycompany.customerservice.model.Customer;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends CrudRepository<Customer, Integer> {
+}
