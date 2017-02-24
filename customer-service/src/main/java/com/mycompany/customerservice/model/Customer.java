267fb01fc9db93d53e52bcb1c99036745b5becea
Colin But <colinbut@users.noreply.github.com>
2017-02-24 21:36:36 +0000

    model

---
+++ customer-service/src/main/java/com/mycompany/customerservice/model/Customer.java
@@ -0,0 +1,20 @@
+/*
+ * |-------------------------------------------------
+ * | Copyright © 2017 Colin But. All rights reserved. 
+ * |-------------------------------------------------
+ */
+package com.mycompany.customerservice.model;
+
+import lombok.Data;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+@Data
+public class Customer {
+
+    private int id;
+    private String name;
+    private String address;
+    private String email;
+
+}
