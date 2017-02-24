a218b682b4ffd02bf971bd36d0c25a2653a2bad0
Colin But <colinbut@users.noreply.github.com>
2017-02-24 21:08:54 +0000

    deleted code

--- customer-service/src/main/java/com/mycompany/customerservice/command/CustomerCommand.java
+++
@@ -1,9 +0,0 @@
-/*
- * |-------------------------------------------------
- * | Copyright © 2017 Colin But. All rights reserved. 
- * |-------------------------------------------------
- */
-package com.mycompany.customerservice.command;
-
-public interface CustomerCommand {
-}
