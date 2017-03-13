a465ef82c9a00d4459c2e75aac8a1d6baac13dc2
Colin But <colinbut@users.noreply.github.com>
2017-03-13 20:45:08 +0000

    added more CRUD methods in user service

--- user-service/src/main/java/com/mycompany/userservice/UserService.java
+++ user-service/src/main/java/com/mycompany/userservice/UserService.java
@@ -5,7 +5,17 @@
  */
 package com.mycompany.userservice;
 
+import java.util.List;
+
 public interface UserService {
 
+    List<User> getUsers();
+
+    User getUser(int userId);
+
     void addUser(User user);
+
+    void updateUser(User user);
+
+    void removeUser(int userId);
 }
