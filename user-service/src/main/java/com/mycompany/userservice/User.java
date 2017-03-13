1fb8bf741fa55b60a8670cba204969a06e16c854
Colin But <colinbut@users.noreply.github.com>
2017-03-13 20:40:13 +0000

    User Service microservice

---
+++ user-service/src/main/java/com/mycompany/userservice/User.java
@@ -0,0 +1,68 @@
+/*
+ * |-------------------------------------------------
+ * | Copyright © 2017 Colin But. All rights reserved. 
+ * |-------------------------------------------------
+ */
+package com.mycompany.userservice;
+
+import java.util.Objects;
+
+public class User {
+
+    private String username;
+    private String password;
+    private String emailAddress;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return Objects.equals(username, user.username) &&
+            Objects.equals(password, user.password) &&
+            Objects.equals(emailAddress, user.emailAddress);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(username, password, emailAddress);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("User{");
+        sb.append("username='").append(username).append('\'');
+        sb.append(", password='").append(password).append('\'');
+        sb.append(", emailAddress='").append(emailAddress).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+}
---
+++ user-service/src/main/java/com/mycompany/userservice/UserService.java
@@ -0,0 +1,11 @@
+/*
+ * |-------------------------------------------------
+ * | Copyright © 2017 Colin But. All rights reserved. 
+ * |-------------------------------------------------
+ */
+package com.mycompany.userservice;
+
+public interface UserService {
+
+    void addUser(User user);
+}
