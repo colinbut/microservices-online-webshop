39b448627e2240c10efe05a7561e2d3aa9f4fab8
Colin But <colinbut@users.noreply.github.com>
2017-03-21 21:51:55 +0000

    UserService DAOs and model update based on database

--- user-service/src/main/java/com/mycompany/userservice/dao/impl/UserDaoImpl.java
+++ user-service/src/main/java/com/mycompany/userservice/dao/impl/UserDaoImpl.java
@@ -7,33 +7,95 @@ package com.mycompany.userservice.dao.impl;
 
 import com.mycompany.userservice.dao.UserDao;
 import com.mycompany.userservice.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 @Repository
 public class UserDaoImpl implements UserDao {
 
-    List<User> users = new ArrayList<>();
+    private static final Logger LOG = LoggerFactory.getLogger(UserDaoImpl.class);
+
+    private static final String FIND_ALL_USERS_SQL = "SELECT * FROM USERS";
+    private static final String FIND_USER_SQL = "SELECT * FROM USERS WHERE id = ?";
+    private static final String UPDATE_USER_SQL = "UPDATE USER SET firstname = ?, lastname = ?, username = ?, " +
+        "password = ?, email = ? where id = ?";
+    private static final String DELETE_USER_SQL = "DELETE FROM USER WHERE id = ?";
+
+    private UserRowMapper userRowMapper = new UserRowMapper();
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
     @Override
     public List<User> findUsers() {
+        List<User> users = jdbcTemplate.query(FIND_ALL_USERS_SQL, userRowMapper);
+
+        LOG.info("Retrieved list of users from database: {}", users);
+
         return users;
     }
 
     @Override
     public User findUser(int userId) {
-        return users.get(userId);
+
+        User user = jdbcTemplate.queryForObject(
+            FIND_USER_SQL, new Object[] {userId}, userRowMapper
+        );
+
+        LOG.info("Retrieved user with id {} from database: {}", userId, user);
+
+        return user;
     }
 
     @Override
     public void saveUser(User user) {
-        users.add(user);
+        LOG.info("Updating user in database with id {}:", user.getId());
+
+        int updateCount = jdbcTemplate.update(UPDATE_USER_SQL,
+            user.getFirstname(), user.getLastname(), user.getUsername(), user.getPassword(), user.getEmailAddress(),
+            user.getId());
+
+        if (updateCount == 1) {
+            LOG.info("Successfully updated database for user with id {}: ", user.getId());
+        }
+
     }
 
     @Override
     public void deleteUser(int userId) {
-        users.remove(userId);
+        LOG.info("Deleting user in database with id {}:", userId);
+
+        int deleteCount = jdbcTemplate.update(DELETE_USER_SQL, userId);
+
+        if (deleteCount == 1) {
+            LOG.info("Successfully deleted user with id: {} from database", userId);
+        }
+    }
+
+    private class UserRowMapper implements RowMapper<User> {
+
+        @Override
+        public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+            return mapUser(rs);
+        }
+
+        private User mapUser(ResultSet rs) throws SQLException {
+            User user = new User();
+            user.setId(rs.getInt("id"));
+            user.setFirstname(rs.getString("firstname"));
+            user.setLastname(rs.getString("lastname"));
+            user.setUsername(rs.getString("username"));
+            user.setPassword(rs.getString("password"));
+            user.setEmailAddress(rs.getString("email"));
+            return user;
+        }
     }
 }
--- user-service/src/main/java/com/mycompany/userservice/model/User.java
+++ user-service/src/main/java/com/mycompany/userservice/model/User.java
@@ -9,9 +9,36 @@ import java.util.Objects;
 
 public class User {
 
+    private int id;
+    private String firstname;
+    private String lastname;
     private String username;
     private String password;
     private String emailAddress;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
 
     public String getUsername() {
         return username;
@@ -38,6 +65,19 @@ public class User {
     }
 
     @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("User{");
+        sb.append("id=").append(id);
+        sb.append(", firstname='").append(firstname).append('\'');
+        sb.append(", lastname='").append(lastname).append('\'');
+        sb.append(", username='").append(username).append('\'');
+        sb.append(", password='").append(password).append('\'');
+        sb.append(", emailAddress='").append(emailAddress).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+
+    @Override
     public boolean equals(Object o) {
         if (this == o) {
             return true;
@@ -46,23 +86,17 @@ public class User {
             return false;
         }
         User user = (User) o;
-        return Objects.equals(username, user.username) &&
+        return id == user.id &&
+            Objects.equals(firstname, user.firstname) &&
+            Objects.equals(lastname, user.lastname) &&
+            Objects.equals(username, user.username) &&
             Objects.equals(password, user.password) &&
             Objects.equals(emailAddress, user.emailAddress);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(username, password, emailAddress);
+        return Objects.hash(id, firstname, lastname, username, password, emailAddress);
     }
 
-    @Override
-    public String toString() {
-        final StringBuilder sb = new StringBuilder("User{");
-        sb.append("username='").append(username).append('\'');
-        sb.append(", password='").append(password).append('\'');
-        sb.append(", emailAddress='").append(emailAddress).append('\'');
-        sb.append('}');
-        return sb.toString();
-    }
 }
