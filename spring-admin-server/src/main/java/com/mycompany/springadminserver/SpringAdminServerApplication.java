c9fabc9ae734ea62f195b84dd18c27f196af9caa
Colin But <colin.but@outlook.com>
2022-01-14 16:21:55 +0000

    Adding spring admin server

---
+++ spring-admin-server/src/main/java/com/mycompany/springadminserver/SpringAdminServerApplication.java
@@ -0,0 +1,13 @@
+package com.mycompany.springadminserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAdminServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAdminServerApplication.class, args);
+	}
+
+}
