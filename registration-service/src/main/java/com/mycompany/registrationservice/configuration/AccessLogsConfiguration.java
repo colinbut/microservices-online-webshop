66cdb1887f7529e0e2c24f8b782666d6c517e220
Colin But <colin.but@outlook.com>
2022-02-15 20:30:33 +0000

    sending tomcat access logs as json to stdout

---
+++ registration-service/src/main/java/com/mycompany/registrationservice/configuration/AccessLogsConfiguration.java
@@ -0,0 +1,22 @@
+package com.mycompany.registrationservice.configuration;
+
+import ch.qos.logback.access.tomcat.LogbackValve;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AccessLogsConfiguration {
+
+    @Bean
+    public WebServerFactoryCustomizer<TomcatServletWebServerFactory> acessLogsCustomizer() {
+        return factory -> {
+            var logbackValue = new LogbackValve();
+            logbackValue.setFilename("logback-access.xml");
+            logbackValue.setAsyncSupported(true);
+            factory.addContextValves(logbackValue);
+        };
+    }
+
+}
